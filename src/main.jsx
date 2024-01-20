29da55072c7c524112a78e7911f61fae868a3e56
Vibhour Sharma <vibhour121@gmail.com>
2024-01-20 23:59:27 +0530

    🆕New: Router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SearchResult from "./pages/SearchResult.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/search",
+    element: <SearchResult />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);

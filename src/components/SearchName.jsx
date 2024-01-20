6bc66aa3f9ff476b8f0584afd9000e33116a3d37
Vibhour Sharma <vibhour121@gmail.com>
2024-01-21 05:21:55 +0530

    🚀Update: Searched item name updated

--- src/components/SearchName.jsx
+++ src/components/SearchName.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const SearchName = () => {
+const SearchName = ({ search }) => {
   return (
     <div className="flex items-center justify-center text-[#ffffff]">
       <div className="w-[401px] h-[77px]">
         <p className="font-[700] text-[35px] text-center bg-transparent leading-[2.2]">
-          Results: Technology
+          Results: {search}
         </p>
       </div>
     </div>

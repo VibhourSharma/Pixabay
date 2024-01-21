23f1cf064e744eef56c864b2a22d5c7f4814eec4
Vibhour Sharma <vibhour121@gmail.com>
2024-01-21 11:05:47 +0530

    ⚙️Feature: Skeleton Loading Added ✅

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,41 +1,63 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import ImageDetail from "../pages/ImageDetail";
+import { useState, useEffect } from "react";
 
-const CardSection = ({ searchResults, search }) => {
+const CardSection = ({ searchResults, searchTerm }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="flex items-center p-12 gap-8 justify-around flex-wrap bg-[#ffffff]">
-      {searchResults.map((results) => {
-        return (
-          <>
-            <Link to={`/search/${search}/${results.id}`}>
-              <div
-                className="w-[364.59px] h-[278.15px] flex flex-col justify-center cursor-pointer"
-                key={results.id}
-              >
+    <>
+      {loading ? (
+        <div className="flex items-center p-12 gap-8 justify-around flex-wrap bg-[#ffffff]">
+          {[1, 2, 3, 4, 5, 6].map((placeholderId) => (
+            <div
+              key={placeholderId}
+              className="w-[364.59px] h-[278.15px] flex flex-col justify-center bg-[#F0F0F0] animate-pulse"
+            />
+          ))}
+        </div>
+      ) : searchResults.length === 0 ? (
+        <div className="text-center w-full flex items-center justify-center mt-16">
+          <p className="p-8 w-[30%] rounded-lg backdrop-blur-xl bg-[#D9D9D91D] border-[#B6B6B66E] border-[4px] text-[#ffffff] text-3xl">
+            {" "}
+            No results found!
+          </p>
+        </div>
+      ) : (
+        <div className="flex items-center p-12 gap-8 justify-evenly flex-wrap bg-[#ffffff]">
+          {searchResults.map((results) => (
+            <Link to={`/search/${searchTerm}/${results.id}`} key={results.id}>
+              <div className="w-[364.59px] h-[278.15px] flex flex-col justify-center cursor-pointer">
                 <img
                   src={results.webformatURL}
                   alt={`Image number: ${results.id}`}
                   className="h-[242.61px]"
                 />
                 <div className="flex h-full w-full items-center gap-[7.1px]">
-                  {results.tags.split(",").map((tag) => {
-                    return (
-                      <span
-                        className="rounded-[1.78px] pr-[15.8px] pl-[7.1px] mt-2 bg-[#F5F5F5]"
-                        key={tag}
-                      >
-                        {tag.trim()}
-                      </span>
-                    );
-                  })}
+                  {results.tags.split(",").map((tag) => (
+                    <span
+                      className="rounded-[1.78px] p-1 mt-2 bg-[#F5F5F5] text-sm"
+                      key={tag}
+                    >
+                      {tag.trim()}
+                    </span>
+                  ))}
                 </div>
               </div>
             </Link>
-          </>
-        );
-      })}
-    </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/ImageDetail.jsx
+++ src/pages/ImageDetail.jsx
@@ -2,33 +2,100 @@ import { useEffect, useState } from "react";
 import CrossIcon from "../assets/CrossIcon.png";
 import { useNavigate, useParams } from "react-router-dom";
 
-const ImageDetail = ({ closeImageDetails }) => {
+const ImageDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   function goBack() {
     navigate(-1);
   }
   const [idData, setIdData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const apiUrl = "https://pixabay.com/api/";
     const apiKey = import.meta.env.VITE_PIXABAY_API_KEY;
+
+    setLoading(true);
+
     fetch(`${apiUrl}?key=${apiKey}&id=${id}`)
       .then((res) => res.json())
       .then((imageIdData) => {
         setIdData(imageIdData.hits);
-        console.log(idData);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
       });
   }, [id]);
 
-  useEffect(() => {
-    console.log(idData);
-  }, [idData]);
-
   return (
     <>
-      {idData.map((dataId) => {
-        return (
-          <div className="fixed top-0 left-0 w-full h-full bg-[#000000] bg-opacity-5 flex items-center justify-center">
+      {loading ? (
+        <div className="fixed top-0 left-0 w-full h-full bg-[#000000] bg-opacity-5 flex items-center justify-center">
+          <div className="w-[1217.32px] h-[650px] bg-[#ffffff] rounded-[8.89px]">
+            <div className="w-full h-[76.42px] bg-[#F5F5F5] p-8 rounded-[8.89px] animate-pulse" />
+            <div className="w-full p-4 flex justify-evenly">
+              <div className="w-[780px] h-[480px] rounded-[7.11px] bg-[#F0F0F0] animate-pulse" />
+              <div>
+                <div>
+                  <span className="w-[103px] h-[52px] font-[500] text-[21.33px] leading-[51.45px] bg-[#F0F0F0] animate-pulse" />
+                  <div className="w-[275.45px] h-[163.73px] text-[14px]">
+                    <div className="border-[0.89px] border-[#DEE8F4] w-[275.45px] h-[41.11px] flex items-center justify-between p-4 bg-[#F0F0F0] animate-pulse" />
+                    <div className="border-[0.89px] border-[#DEE8F4] w-[275.45px] h-[41.11px] flex items-center justify-between p-4 bg-[#F0F0F0] animate-pulse" />
+                    <div className="border-[0.89px] border-[#DEE8F4] w-[275.45px] h-[41.11px] flex items-center justify-between p-4 bg-[#F0F0F0] animate-pulse" />
+                    <div className="border-[0.89px] border-[#DEE8F4] w-[275.45px] h-[41.11px] flex items-center justify-between p-4 bg-[#F0F0F0] animate-pulse" />
+                  </div>
+                  <button className="w-[274.67px] h-[37.33px] rounded-[4.44px] bg-[#F0F0F0] animate-pulse" />
+                </div>
+                <div className="w-[275.45px] h-[163.73px] text-[14px] mt-2">
+                  <span className="w-[103px] h-[52px] font-[500] text-[21.33px] leading-[51.45px] bg-[#F0F0F0] animate-pulse" />
+                  <div className="flex items-center justify-between mt-2">
+                    <span className="flex flex-col">
+                      User
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                    <span className="flex flex-col">
+                      UserID
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                    <span className="flex flex-col">
+                      Type
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                  </div>
+                  <div className="flex items-center justify-between mt-5">
+                    <span className="flex flex-col">
+                      Views
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                    <span className="flex flex-col">
+                      Downloads
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                    <span className="flex flex-col">
+                      Likes
+                      <span className="font-[600] text-[15.99px] leading-[23.1px] bg-[#F0F0F0] animate-pulse" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full h-8 flex items-center justify-center">
+              <div className="w-[90%] h-full flex items-center">
+                <span className="font-[700] text-[17.77px] bg-[#F0F0F0] animate-pulse" />
+                <div className="flex h-full w-full items-center gap-[7.1px]">
+                  <span className="rounded-[1.78px] pr-[15.8px] pl-[7.1px] bg-[#F5F5F5] animate-pulse" />
+                  <span className="rounded-[1.78px] pr-[15.8px] pl-[7.1px] bg-[#F5F5F5] animate-pulse" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        idData.map((dataId) => (
+          <div
+            key={dataId.id}
+            className="fixed top-0 left-0 w-full h-full bg-[#000000] bg-opacity-5 flex items-center justify-center"
+          >
             <div className="w-[1217.32px] h-[650px] bg-[#ffffff] rounded-[8.89px]">
               <div className="flex items-center justify-between w-full h-[76.42px] bg-[#F5F5F5] p-8 rounded-[8.89px]">
                 <div className="text-[21.33px] font-[500]">
@@ -154,8 +221,8 @@ const ImageDetail = ({ closeImageDetails }) => {
               </div>
             </div>
           </div>
-        );
-      })}
+        ))
+      )}
     </>
   );
 };

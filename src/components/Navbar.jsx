092176d7cadcac10ad56c785b1c36d7a1721fe7e
Vibhour Sharma <vibhour121@gmail.com>
2024-01-21 11:07:56 +0530

    ⚒️New: Added Responsiveness✅

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-      <div className="flex items-center justify-center text-[#FFFFFF]">
-        <div className="flex items-center justify-between p-8 rounded-[8.91px] backdrop-blur-xl bg-[#D9D9D91D] border-[#B6B6B66E]  h-[67.69px] w-[1217.48px] border-[4px] mt-8">
-          <div className="font-[600]">Homepage</div>
-          <div className="w-56 flex items-center justify-between">
+      <div className="flex items-center justify-center text-[#FFFFFF] md:text-[13px]">
+        <div className="flex items-center justify-between p-8 rounded-[8.91px] backdrop-blur-xl bg-[#D9D9D91D] border-[#B6B6B66E] h-[60px] w-[1217.48px] border-[4px] mt-8 sm:w-[400px] md:w-[100%] lg:w-[100%] xl:w-[700px] xxl:w-[900px]">
+          <Link to="/">
+            <div className="font-[600] ">Homepage</div>
+          </Link>
+          <div className="w-56 flex items-center justify-between md:justify-end md:gap-3 lg:justify-end lg:gap-2">
             <span className="font-[600] leading-[19.95px]">LogIn</span>
-            <button className="w-[162.98px] h-[34.73px] rounded-[8.91px] border-[2.67px] leading-[19.95px] font[600]">
+            <button className="w-[162.98px] h-[34.73px] rounded-[8.91px] border-[2.67px] leading-[19.95px] font[600] md:w-28 lg:w-32">
               Create Account
             </button>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xxl: { max: "1300px" },
+
+      xl: { max: "1100px", min: "800px" },
+
+      lg: { max: "800px", min: "600px" },
+
+      md: { max: "600px", min: "453px" },
+
+      sm: { max: "453px", min: "360px" },
+    },
   },
   plugins: [],
 };

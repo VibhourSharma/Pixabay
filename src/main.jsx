be244ef84611763fbd3de4c224c417ed32d34b9d
Vibhour Sharma <vibhour121@gmail.com>
2024-03-05 23:43:19 +0530

    🚀Feat: Random images on dashboard added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import HomePage from "./pages/HomePage.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SearchResult from "./pages/SearchResult.jsx";
 import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const router = createBrowserRouter([
   {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
+import React, { useState, useEffect } from "react";
 import { Hero, Navbar, Search, Trending } from "../components";
+import bg1 from "../assets/bgImages/bg1.jpg";
+import bg2 from "../assets/bgImages/bg2.jpg";
+import bg3 from "../assets/bgImages/bg3.jpg";
+import bg4 from "../assets/bgImages/bg4.jpg";
+import bg5 from "../assets/bgImages/bg5.jpg";
+import bg6 from "../assets/bgImages/bg6.jpg";
 
 function HomePage() {
+  const backgrounds = [bg1, bg2, bg3, bg4, bg5, bg6];
+  const [currentBackground, setCurrentBackground] = useState(bg1);
+
+  useEffect(() => {
+    const interval = setInterval(changeBackground, 6000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const changeBackground = () => {
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+    setCurrentBackground(backgrounds[randomIndex]);
+  };
+
   return (
-    <div className="flex items-center justify-center flex-col w-full h-full">
+    <div
+      className="flex items-center flex-col w-full bg-no-repeat bg-cover min-h-screen"
+      style={{
+        backgroundImage: `url(${currentBackground})`,
+        transition: "background-image 2s ease",
+      }}
+    >
       <Navbar />
       <Hero />
       <Search />

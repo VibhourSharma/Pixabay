e9c0ad98af67b289570c3b8bcb9e388bfebb4ca6
Vibhour Sharma <vibhour121@gmail.com>
2024-03-10 02:08:30 +0530

    🚀Fix: No results page

--- src/components/Search/CardSection.jsx
+++ src/components/Search/CardSection.jsx
@@ -1,10 +1,9 @@
-import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import CardLayout from "./CardLayout";
+import sad from "../../assets/sad.svg";
 
 const CardSection = ({ searchResults }) => {
   const [loading, setLoading] = useState(true);
-  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,19 +17,18 @@ const CardSection = ({ searchResults }) => {
   return (
     <>
       {loading ? (
-        <div className="flex items-center p-8 gap-4 flex-wrap justify-center bg-[#ffffff]">
+        <div className="flex items-center p-8 gap-4 flex-wrap justify-center bg-white">
           {[1, 2, 3, 4, 5, 6, 7, 8].map((placeholderId) => (
             <div
               key={placeholderId}
-              className="w-[364.59px] h-[278.15px] flex flex-col justify-center bg-gray-300 animate-pulse"
+              className="w-[350px] h-[250px] flex flex-col justify-center bg-gray-300 animate-pulse"
             />
           ))}
         </div>
       ) : searchResults.length === 0 ? (
-        <div className="text-center w-full flex items-center justify-center mt-16">
-          <p className="p-8 w-[30%] rounded-lg backdrop-blur-xl bg-[#D9D9D91D] border-[#B6B6B66E] border-[4px] text-[#ffffff] text-3xl">
-            No results found!
-          </p>
+        <div className="text-center bg-white w-full flex items-center text-black flex-col">
+          <img src={sad} />
+          <p className="text-xl font-semibold mt-4">No results found!</p>
         </div>
       ) : (
         <CardLayout searchResults={searchResults} />

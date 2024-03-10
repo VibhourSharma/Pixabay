b805e9e085785b96ccea5e5cf25abc4f9c7801f5
Vibhour Sharma <vibhour121@gmail.com>
2024-03-11 01:34:51 +0530

    🚀Fix: Similar Tag's default search

--- src/components/Search/SimilarTags.jsx
+++ src/components/Search/SimilarTags.jsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const SimilarTags = ({ tags }) => {
+  const [searchParams] = useSearchParams();
+  const searchType = searchParams.get("type");
+
   return (
     <>
-      <div className="flex items-center px-10 w-full  bg-white overflow-hidden gap-x-2 overflow-x-scroll no-scrollbar">
+      <div className="flex items-center px-10 w-full bg-white overflow-hidden gap-x-2 overflow-x-scroll no-scrollbar">
         {tags.map((name) => {
           return (
             <ul key={name}>
-              <Link to={`/search?search=${name}`}>
+              <Link
+                to={
+                  searchType === "videos"
+                    ? `/search?type=videos&search=${name}`
+                    : `/search?search=${name}`
+                }
+              >
                 <li className="p-4 h-[30px] flex justify-center items-center border-[#D1D1D1] border-[2px] rounded-md cursor-pointer hover:border-black transition-all text-sm">
                   {name}
                 </li>

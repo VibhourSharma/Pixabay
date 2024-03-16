595faff8573c05b627bd9b3324753aaa4e8d6c6f
Vibhour Sharma <vibhour121@gmail.com>
2024-03-16 22:53:35 +0530

    🚀New: Added new routes for new pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SearchResult from "./pages/SearchResult.jsx";
-import "./App.css";
+import Favourites from "./pages/Favourites.jsx";
+import Downloads from "./pages/Downloads.jsx";
+import { FirebaseProvider } from "./context/Firebase.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,8 +19,26 @@ const router = createBrowserRouter([
     path: "/search",
     element: <SearchResult />,
   },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/favourites",
+    element: <Favourites />,
+  },
+  {
+    path: "/downloads",
+    element: <Downloads />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <FirebaseProvider>
+    <RouterProvider router={router} />
+  </FirebaseProvider>
 );

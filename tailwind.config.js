6264c0179c2402b5bf5fe2d73a2a4d62e69efdf9
Vibhour Sharma <vibhour121@gmail.com>
2024-03-18 01:14:18 +0530

    ⚙️Updated custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,7 @@ export default {
   theme: {
     extend: {},
     screens: {
-      xxl: { max: "1300px" },
-
-      xl: { max: "1100px", min: "800px" },
-
-      lg: { max: "800px", min: "600px" },
-
-      md: { max: "600px", min: "453px" },
-
-      sm: { max: "453px", min: "360px" },
+      sm: { max: "453px", min: "355px" },
     },
   },
   plugins: [],
